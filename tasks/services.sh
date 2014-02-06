a39c928e754bc8665b82cc1bda473fc884712436
Tonko Mulder <tonko@tonkomulder.nl>
2014-02-06 17:01:18 +0100

    added nginx, mariadb, php-fpm as services to be started

--- tasks/services.sh
+++ tasks/services.sh
@@ -1,2 +1,14 @@
 #/usr/bin/env bash
 
+echo "Enable and start nginx"
+systemctl enable nginx
+systemctl start nginx
+
+echo "Enable and start MariaDB"
+systemctl enable mysqld
+systemctl start mysqld
+
+echo "Enable and start php-fpm"
+systemctl enable php-fpm
+systemctl start php-fpm
+
