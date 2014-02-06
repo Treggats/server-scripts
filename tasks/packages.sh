7fe5b8fd0a61e47f13121eb2a0f538bd077cb9a7
Tonko Mulder <tonko@tonkomulder.nl>
2014-02-06 18:29:39 +0100

    removed pacman's pretend option

--- tasks/packages.sh
+++ tasks/packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Adding packages"
-for pkg in $(cat packages.txt); do pacman -Sp --noprogressbar --noconfirm $pkg; done
+for pkg in $(cat packages.txt); do pacman -S --noprogressbar --noconfirm $pkg; done
